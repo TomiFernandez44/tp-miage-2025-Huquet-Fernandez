f3f62c7e85a8c9096db74df9ff0941eabe5972a1
tomifernandez <tomifernand@gmail.com>
2025-03-13 15:19:30 +0100

    Exercice 1

--- todolist-debut-ex1/src/main/java/com/acme/todolist/TodoListController.java
+++ todolist-debut-ex1/src/main/java/com/acme/todolist/TodoListController.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.inject.Inject;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +25,7 @@ public class TodoListController {
 	private static final String LATE = "[LATE!]";
 	private TodoItemRepository todoItemRepository;
 
+	@Inject
 	public TodoListController(TodoItemRepository todoItemRepository) {
 		super();
 		this.todoItemRepository = todoItemRepository;
@@ -36,8 +38,8 @@ public class TodoListController {
 	@PostMapping("/todos")
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public void createTodoItem(@RequestBody TodoItem todoItem) {
-		// Code à compléter
-		// ...
+		this.todoItemRepository.save(todoItem);
+
 	}
 
 	@GetMapping("/todos")
